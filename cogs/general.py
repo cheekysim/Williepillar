ad4cd0c17e71f3d590d6d3a8a62b9de8dd66a3f3
cheekysim <minecraftcheekysim@gmail.com>
2022-07-11 00:01:27 +0100

    fixed embeds

--- cogs/general.py
+++ cogs/general.py
@@ -1,8 +1,11 @@
 import discord
 from discord.ext import commands
 from discord.commands import slash_command, Option
-from modules.embed import embed
 import json
+
+import os, sys, inspect
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
+from modules.embed import embed
 
 with open('config.json') as f:
     data = json.load(f)
