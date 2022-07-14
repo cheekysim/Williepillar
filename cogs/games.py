1bb149f308815811505ba7c677606de5fdbead2f
DevilWasTaken <youshalev6@gmail.com>
2022-07-14 08:08:37 +0300

    Tic Tac Toe Complete

--- cogs/games.py
+++ cogs/games.py
@@ -1,3 +1,4 @@
+from code import interact
 from pydoc import describe
 import discord
 from discord.ext import commands
@@ -5,16 +6,26 @@ from discord.commands import slash_command, Option
 import json
 from discord.ui import Button, View
 import random
+import os, sys, inspect
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
+from modules.embed import embed
 
 
 with open('config.json') as f:
     data = json.load(f)
     guilds = data["guilds"]
 
-embed = discord.Embed(title="Player0's Turn",colour=0x71368a)
+
 flag = 0
+score1 = 0
+score2 = 0
+turn = 1
+p = None
+re = 0
 player1 = None
 player2 = None
+winState = False
+bList = []
 
 class Games(commands.Cog):
     def __init__(self, bot):
@@ -22,53 +33,210 @@ class Games(commands.Cog):
     
     @slash_command(name="tictactoe", description="A game of TicTacToe", guild_ids=[703637471212077096])
     async def tictactoe(self, ctx, user: Option(discord.User, "User", required=True)):
-        player = ctx.author
+        global turn, winState, re,p,score1,score2
+        score1 = 0
+        score2 = 0
+        p = None
+        winState = False
+        turn = 1
+        re = 0
+        player1 = ctx.author.name
+        player2 = user.name
+        embed = discord.Embed(title="Player0's Turn",description=f"**{player1}: {score1}  {player2}: {score2}**",colour=0x71368a)
         class TicTacToe(View):
             def __init__(self): 
                 super().__init__()
                 rnd = random.choice([1,2])
+                global flag
                 if rnd == 1:
-                    player1 = user
-                    player2 = player
-                    embed.title = f"{player1}'s Turn"
+                    flag = 0
+                    embed.title = f"{player1}'s Turn [X]"
                 elif rnd == 2:
-                    player1 = player
-                    player2 = user
-                    embed.title = f"{player1}'s Turn"
-                button1 = Button(label="*", style=discord.ButtonStyle.gray, row = 0, custom_id="0")
-                button2 = Button(label="*", style=discord.ButtonStyle.gray, row = 0, custom_id="1")
-                button3 = Button(label="*", style=discord.ButtonStyle.gray, row = 0, custom_id="2")
-                button4 = Button(label="*", style=discord.ButtonStyle.gray, row = 1, custom_id="3")
-                button5 = Button(label="*", style=discord.ButtonStyle.gray, row = 1, custom_id="4")
-                button6 = Button(label="*", style=discord.ButtonStyle.gray, row = 1, custom_id="5")
-                button7 = Button(label="*", style=discord.ButtonStyle.gray, row = 2, custom_id="6")
-                button8 = Button(label="*", style=discord.ButtonStyle.gray, row = 2, custom_id="7")
-                button9 = Button(label="*", style=discord.ButtonStyle.gray, row = 2, custom_id="8")
-                bList = [button1,button2,button3,button4,button5,button6,button7,button8,button9]
-                for i in range(9):
+                    flag = 1
+                    embed.title = f"{player2}'s Turn [O]"
+                b1 = Button(label="*", style=discord.ButtonStyle.gray, row = 0, custom_id="0")
+                b2 = Button(label="*", style=discord.ButtonStyle.gray, row = 0, custom_id="1")
+                b3 = Button(label="*", style=discord.ButtonStyle.gray, row = 0, custom_id="2")
+                b4 = Button(label="*", style=discord.ButtonStyle.gray, row = 1, custom_id="3")
+                b5 = Button(label="*", style=discord.ButtonStyle.gray, row = 1, custom_id="4")
+                b6 = Button(label="*", style=discord.ButtonStyle.gray, row = 1, custom_id="5")
+                b7 = Button(label="*", style=discord.ButtonStyle.gray, row = 2, custom_id="6")
+                b8 = Button(label="*", style=discord.ButtonStyle.gray, row = 2, custom_id="7")
+                b9 = Button(label="*", style=discord.ButtonStyle.gray, row = 2, custom_id="8")
+                bR = Button(label="Reamtch 0/2", style=discord.ButtonStyle.green,row = 3)
+                async def rematch(interaction):
+                    global p
+                    global re
+                    if re != 1:
+                        p = interaction.user
+                        re += 1
+                        bR.label = f"Rematch {re}/2"
+                        await interaction.response.edit_message(embed = embed,view = self)
+                    elif re == 1 and interaction.user != p:
+                        view = TicTacToe()
+                        global turn,score1,score2
+                        turn = 1
+                        re = 0
+                        p = None
+                        embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                        await interaction.response.edit_message(embed = embed,view = view)
+                    elif interaction.user == p:
+                        await interaction.response.send_message("You already voted for a rematch", ephemeral=True)
+                bR.callback = rematch
+                bE = Button(label="Exit", style=discord.ButtonStyle.red, row = 3)
+                global bList
+                bList = [b1,b2,b3,b4,b5,b6,b7,b8,b9,bE,bR]
+                async def exit(interaction):
+                    embed.title = "Exited Game."
+                    await interaction.response.edit_message(view=None, embed=embed)
+                bE.callback = exit
+                for i in range(10):
                     self.add_item(bList[i])
                 async def button_callback(interaction):
                     button = bList[int(interaction.custom_id)]
-                    global flag
-                    if flag == 1:
-                        button.label = "X"
-                        flag = 2
-                        button.disabled = True
-                        embed.title = f"{player1}'s Turn"
-                    elif flag == 1:
-                        button.label = "O"
-                        flag = 0
-                        button.disabled = True
-                        embed.title = f"{player2}'s Turn"
-                    await interaction.response.edit_message(view=self, embed=embed)
+                    global turn
+                    turn += 1
+                    OX = ["X","O"]
+                    global winState
+                    if winState == False:
+                        global flag            
+                        if flag == 0:
+                            button.label = "X"
+                            flag = 1
+                            button.disabled = True
+                            embed.title = f"{player2}'s Turn [O]"
+                            pass
+                        elif flag == 1:
+                            button.label = "O"
+                            flag = 0
+                            button.disabled = True
+                            embed.title = f"{player1}'s Turn [X]"
+                    if turn > 9:
+                        embed.title = "It's A Tie!"
+                        self.add_item(bR)
+                        await interaction.response.edit_message(view=self,embed=embed)
+                    for char in OX:
+                        if b1.label == char and b2.label == char and b3.label == char:
+                            if char == "O":
+                                global score2,score1
+                                embed.title = f"{player2} Is The Winner!"
+                                score2 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            else:
+                                embed.title = f"{player1} Is The Winner!"
+                                score1 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            winState = True
+                            for l in range(9):
+                                bList[l].disabled
+                        elif b4.label == char and b5.label == char and b6.label == char:
+                            if char == "O":
+                                embed.title = f"{player2} Is The Winner!"
+                                score2 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            else:
+                                embed.title = f"{player1} Is The Winner!"
+                                score1 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            winState = True
+                            for l in range(9):
+                                bList[l].disabled
+                        elif b7.label == char and b8.label == char and b9.label == char:
+                            if char == "O":
+                                embed.title = f"{player2} Is The Winner!"
+                                score2 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            else:
+                                embed.title = f"{player1} Is The Winner!"
+                                score1 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            winState = True
+                            for l in range(9):
+                                bList[l].disabled
+                        elif b1.label == char and b4.label == char and b7.label == char:
+                            if char == "O":
+                                embed.title = f"{player2} Is The Winner!"
+                                score2 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            else:
+                                embed.title = f"{player1} Is The Winner!"
+                                score1 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            winState = True
+                            for l in range(9):
+                                bList[l].disabled
+                        elif b2.label == char and b5.label == char and b8.label == char:
+                            if char == "O":
+                                embed.title = f"{player2} Is The Winner!"
+                                score2 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            else:
+                                embed.title = f"{player1} Is The Winner!"
+                                score1 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            winState = True
+                            for l in range(9):
+                                bList[l].disabled
+                        elif b3.label == char and b6.label == char and b9.label == char:
+                            if char == "O":
+                                embed.title = f"{player2} Is The Winner!"
+                                score2 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            else:
+                                embed.title = f"{player1} Is The Winner!"
+                                score1 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            winState = True
+                            for l in range(9):
+                                bList[l].disabled
+                        elif b1.label == char and b5.label == char and b9.label == char:
+                            if char == "O":
+                                embed.title = f"{player2} Is The Winner!"
+                                score2 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            else:
+                                embed.title = f"{player1} Is The Winner!"
+                                score1 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            winState = True
+                            for l in range(9):
+                                bList[l].disabled
+                        elif b3.label == char and b5.label == char and b7.label == char:
+                            if char == "O":
+                                embed.title = f"{player2} Is The Winner!"
+                                score2 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            else:
+                                embed.title = f"{player1} Is The Winner!"
+                                score1 += 1
+                                embed.description = f"**{player1}: {score1}  {player2}: {score2}**"
+                            winState = True
+                            for l in range(9):
+                                bList[l].disabled
+                    if winState == True:
+                        self.add_item(bR)
+                        winState = False
+                        await interaction.response.edit_message(view=self,embed=embed)
+                    else:
+                        await interaction.response.edit_message(view=self,embed=embed)
                 for j in range(9):
                     bList[j].callback = button_callback
-                async def interaction_check(self, interaction) -> bool:
-                    if interaction.user != self.ctx.author and interaction.user != user:
-                        await interaction.response.send_message("You are not a participant in this game.")
-                        return False
-                    else:
+            async def interaction_check(self, interaction) -> bool:
+                global bList
+
+                if interaction.user != ctx.author and interaction.user != user:
+                    await interaction.response.send_message("You are not a participant in this game.", ephemeral=True)
+                    return False
+                else:
+                    if interaction.user == ctx.author and flag == 0:
                         return True
+                    elif interaction.user == user and flag == 1:
+                        return True
+                    elif (interaction.custom_id == bList[9].custom_id or interaction.custom_id == bList[10].custom_id) and (interaction.user == ctx.author or interaction.user == user):
+                        return True
+                    else:
+                        await interaction.response.send_message("It's not your turn.", ephemeral=True)
+                        return False
         view = TicTacToe()
         await ctx.respond(embed = embed,view = view)
     
