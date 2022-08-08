6185dcd00aa809a98b211782f1f420965b076596
DevilWasTaken <youshalev6@gmail.com>
2022-08-08 20:59:21 +0300

    Mute Timer

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,18 +1,17 @@
 import inspect
 import json
 import os
+from pydoc import describe
 import sys
 import asyncio
-
 import discord
 from discord import default_permissions
 from discord.commands import slash_command, Option
 from discord.ext import commands
-from discord.ext.commands import MissingPermissions
 
-from modules.embed import embed
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
+from modules.embed import embed
 
 with open('config.json') as f:
     data = json.load(f)
@@ -25,7 +24,7 @@ class Admin(commands.Cog):
 
     @slash_command(name="mute", description="Mutes User", guild_ids=[703637471212077096])
     @default_permissions(mute_members=True)
-    async def mute(self, ctx, user: Option(discord.User, "User", required=True), duration: Option(str, "Duration (s)", default="0"), reason: Option(str, "Reason", default="Muted")):
+    async def mute(self, ctx: commands.Context, user: Option(discord.User, "User", required=True), duration: Option(int, "Duration (s)", default="0"), reason: Option(str, "Reason", default="Muted")):
         prole = False
         for role in ctx.guild.roles:
             if role.name.lower() == "muted":
@@ -37,11 +36,17 @@ class Admin(commands.Cog):
             await ctx.guild.create_role(name="Muted", permissions=discord.Permissions(permissions=2048), color=discord.Color(0x111111))
         role = discord.utils.find(lambda r: r.name == "Muted", ctx.guild.roles)
         await user.add_roles(role, reason=reason)
-        if duration == "0":
-            await ctx.respond(embed=embed(ctx, title=f"Muted {user.name}"))
+        if duration == 0:
+            await ctx.respond(embed=embed(ctx, title=f"Muted {user.name}", footer={"text": reason}))
         else:
-            await ctx.respond(embed=embed(ctx, title=f"Muted {user.name} for {duration} Seconds"))
-            await asyncio.sleep(int(duration))
+            a=embed(ctx, title=f"Muted {user.name} for {(duration)} Seconds", footer=[{'text': f'{ctx.bot.user.name} | Requested By: {ctx.author.name} | {duration}s | Reason = {reason}'}])
+            b = await ctx.respond(embed=embed)
+            for sec in range(duration):
+                a.footer = [{'text': f'{ctx.bot.user.name} | Requested By: {ctx.author.name} | {duration - sec}s'}]
+                await b.edit_original_message(embed=a)
+                await asyncio.sleep(1)
+            a.footer= [{'text': f'{ctx.bot.user.name} | Requested By: {ctx.author.name} | Mute Finished | Reason = {reason}'}]
+            await b.edit_original_message(embed=a)
             await user.remove_roles(role, reason="Auto Un Mute")
 
     @slash_command(name="unmute", description="Un Mutes User", guild_ids=[703637471212077096])
